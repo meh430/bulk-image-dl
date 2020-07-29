cc74d2f959963cc0efe4ab1ce3a665f14beb9a0a
Mehul Pillai <mehulpillai30@gmail.com>
2020-07-29 18:29:41 -0400

    Deletes unused content script

--- scrapeImages.js
+++
@@ -1,9 +0,0 @@
-let imageTags = document.querySelectorAll('img');
-let urls = [];
-
-
-for (let i = 0; i < imageTags.length; i++) {
-    urls.push(imageTags[i].src);
-}
-
-chrome.runtime.sendMessage({ images: urls }, (response) => console.log(response));
