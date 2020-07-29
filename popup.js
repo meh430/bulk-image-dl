82f97bb99ec6738e6f1519318dce33b5d81134ed
Mehul Pillai <mehulpillai30@gmail.com>
2020-07-28 20:57:49 -0400

    Adds popup

--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
+//right click to add image src to list, save list in storage
+//when storage changes, update the list?
 const formats = ["png", "jpg", "gif"];
+let openImages = [];
+let selectedImages = [];
 
 let dlButton = document.getElementById("downloadButton");
 
@@ -20,3 +24,53 @@ dlButton.addEventListener("click", (event) => {
         });
     });
 });
+
+
+function deleteChildren(node) {
+    console.log(node);
+    while (node.firstChild) {
+        node.removeChild(node.lastChild);
+    }
+}
+
+function updateUL(list) {
+    deleteChildren(listCont);
+    let item = {};
+    console.log(list);
+    list.forEach((word) => {
+        item = document.createElement("li");
+        item.className = "itemStyle";
+        item.appendChild(document.createTextNode(word));
+        item.addEventListener("mouseover", (event) => {
+            const clickedElement = event.target || event.srcElement;
+            clickedElement.style.color = "red";
+        });
+        item.addEventListener("mouseout", (event) => {
+            const clickedElement = event.target || event.srcElement;
+            clickedElement.style.color = "white";
+        });
+        item.addEventListener("click", (event) => {
+            const clickedElement = event.target || event.srcElement;
+            const clickedText = clickedElement.textContent;
+            let clickedIndex = -1;
+            if (viewingWords) {
+                clickedIndex = blockedWords.indexOf(clickedText);
+                if (clickedIndex > -1) {
+                    blockedWords.splice(clickedIndex, 1);
+                }
+                updateUL(blockedWords);
+            } else {
+                clickedIndex = blockedUrls.indexOf(clickedText);
+                if (clickedIndex > -1) {
+                    blockedUrls.splice(clickedIndex, 1);
+                }
+                updateUL(blockedUrls);
+            }
+
+            chrome.storage.sync.set({ bWords: blockedWords, bUrls: blockedUrls, globalOff: filterOff }, () => {
+                console.log("Updated deletion");
+            });
+        });
+        listCont.appendChild(item);
+    });
+}
